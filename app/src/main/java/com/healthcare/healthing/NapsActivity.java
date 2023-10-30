28ac892d9ec42c884d9234d13c118a343fdd6de7
HanRoseA <112860641+Hanrosea@users.noreply.github.com>
2023-10-30 22:54:05 +0900

    CJW 헬스장맵 231030

--- app/src/main/java/com/healthcare/healthing/NapsActivity.java
+++ app/src/main/java/com/healthcare/healthing/NapsActivity.java
@@ -1,14 +1,52 @@
 package com.healthcare.healthing;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class NapsActivity extends AppCompatActivity {
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.util.FusedLocationSource;
+
+public class NapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_naps);
+
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().add(R.id.map_fragment, mapFragment).commit();
+        }
+
+        mapFragment.getMapAsync(this);
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
     }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)){
+            if(!locationSource.isActivated()){
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+            return;
+        }super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
 }
